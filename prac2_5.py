0856beebd22e79bcda20f2651cf8ed59c41539ae
Will Dow <s4508@pccs-ttr117.ttr1.ds.cam.ac.uk>
2019-09-10 16:44:32 +0100

    end of tuesday

--- prac2_5.py
+++ prac2_5.py
@@ -8,7 +8,7 @@ def initialBell(x):
 #Put everything inside a main fucntion to avoid global variables
 def main():
     #setup space, initial phi profile and Courant number
-    nx = 40 #number of points in space
+    nx = 100 #number of points in space
     nt = 40 #number of time steps - MODIFIED
     c = 0.20 # The Courant number
     #spatial variable going from zero to one inclusive
@@ -21,6 +21,7 @@ def main():
     phiOld_ftcs = phi_ftcs.copy()
     phiOld_ftbs = phi_ftbs.copy()
     phiOld_ctcs = phi_ctcs.copy()
+    
     
     #FTCS for all time steps looping over space
     for i in range(1,nt):
@@ -36,28 +37,25 @@ def main():
     #FTBS for all time steps, looping over space
     for i in range(1,nt):
         for j in  range(1,nx):
-            phi_ftbs[j] = phiOld_ftcs[j] -c*(phiOld_ftcs[j+1] - phiOld_ftcs[j-1])
+            phi_ftbs[j] = phiOld_ftbs[j] -c*(phiOld_ftbs[j] - phiOld_ftbs[j-1])
         #apply periodic boundary conditions
-        phi_ftcs[0] = phiOld_ftcs[0] - c*(phiOld_ftcs[1] - phiOld_ftcs[nx-1])
-        phi_ftcs[nx] = phi_ftcs[0]
+        phi_ftbs[0] = phiOld_ftbs[0] - c*(phiOld_ftbs[0] - phiOld_ftbs[nx-1])
+        phi_ftbs[nx] = phi_ftbs[0]
         #update phi for the next time-step
-        phiOld_ftcs = phi_ftcs.copy()
+        phiOld_ftbs = phi_ftbs.copy()
         
-    #CTCS for the first time step, looping over space
+    #Using FTCS for the first time step, looping over space
     for j in  range(1,nx):
         phi_ctcs[j] = phiOld_ctcs[j] - 0.5*c*(phiOld_ctcs[j+1] - phiOld_ctcs[j-1])
     #apply periodic boundary conditions
     phi_ctcs[0] = phiOld_ctcs[0] - 0.5*c*(phiOld_ctcs[1] - phiOld_ctcs[nx-1])
     phi_ctcs[nx] = phi_ctcs[0]
-    #update phi for the next time-step
-    phiOld_ctcs = phi_ctcs.copy()
     
-    #Loop over remaining time steps (nt) using CTCS
-    nt_ctcs = 40
-    for n in range (1,nt_ctcs):
+    #Loop over remaining time steps (nt_ctcs) using CTCS
+    for n in range (1,nt):
         #loop over space
         for j in range (1,nx):
-            phiNew_ctcs[j] = phiOld_ctcs[j] - 0.5*c*(phi_ctcs[j+1] - phiOld_ctcs[j-1])
+            phiNew_ctcs[j] = phiOld_ctcs[j] -c*(phi_ctcs[j+1] - phi_ctcs[j-1])
         #apply periodic boundary conditions
         phiNew_ctcs[0] = phiOld_ctcs[0] - c*(phi_ctcs[1] - phi_ctcs[nx-1])
         phiNew_ctcs[nx] = phiNew_ctcs[0]
@@ -70,7 +68,6 @@ def main():
     dx = 1./nx
     dt = c*dx/u
     t = nt*dt
-    
     
     #Plot the solution in comparison to the analyic solution
     plt. plot(x, initialBell(x-u*t), 'k', label = 'analytic')
--- prac2_5_2.py
+++ prac2_5_2.py
@@ -6,16 +6,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def main()
+def main():
     #Setup parameters
     f = 1e-4    #coriolis parameter
-    nt = 12     #number of time steps
-    dt = 5000   #time steps in seconds
+    nt = 500     #number of time steps
+    dt = 500   #time steps in seconds
     #initial conditions
     x0 = 0
     y0 = 1e5
     u0 = 10
-    v = 0
+    v0 = 0
     #initialise velocity from intiial conditions
     u = u0
     v = v0
@@ -27,11 +27,24 @@ def main()
     
     #loop over all times steps
     for n in range (nt):
-        u += dr*f*v
+        u += dt*f*v
         v -= dt*f*u
-        x[n+1] = y[n] + dt*f*v
+        x[n+1] = x[n] + dt*u
         y[n+1] = y[n] + dt*v
         
     #Analytic solution for the location as a function of time
     times = np.linspace(0,nt*dt,nt+1)
-    xa = x0 + 1/f*(u0
+    xa = x0 + 1/f*(u0*np.sin(f*times) - v0*np.cos(f*times)+v0)
+    ya = y0 + 1/f*(u0*np.cos(f*times) + v0*np.sin(f*times)-u0)
+    
+    #Plot the solution in comparison to the analytic solution
+    plt.plot(xa,ya,'-k+', label='analytic')
+    plt.plot(x,y,'-bo',label='forward-backward')
+    plt.legend(loc='best')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    #plt.axhline(0, linestlye=':', color='black')
+    plt.show()
+    
+#Execute
+main()
